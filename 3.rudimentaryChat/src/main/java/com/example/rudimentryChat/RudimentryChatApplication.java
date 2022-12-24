e5d6db913eefdf25e39510060dcb561b0ccb47de
KhaledM0hamed <khaledmzakria1@gmail.com>
2022-12-24 15:54:21 +0200

    init rudimentary chat app

---
+++ 3.rudimentaryChat/src/main/java/com/example/rudimentryChat/RudimentryChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.rudimentryChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RudimentryChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RudimentryChatApplication.class, args);
+	}
+
+}
