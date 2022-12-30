9a2d57668429306ed87c6ca3c32f7c3411a31df9
KhaledM0hamed <khaledmzakria1@gmail.com>
2022-12-30 14:27:31 +0200

    unit testing example

--- 5.testing/src/test/java/com/example/testing/TestingApplicationTests.java
+++ 5.testing/src/test/java/com/example/testing/TestingApplicationTests.java
@@ -1,13 +1,52 @@
 package com.example.testing;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TestingApplicationTests {
 
+	private List<Integer> testList;
+	@BeforeEach
+	public void beforeEach(){
+		testList = new ArrayList<>();
+		testList.add(1);
+		testList.add(2);
+		testList.add(3);
+	}
+	@AfterEach
+	public void afterEach(){
+		testList = null;
+	}
 	@Test
-	void contextLoads() {
+	void testAddZero() {
+		int a = 10;
+		int b = 0;
+		int c = a + b;
+		assertEquals(a, c);
+	}
+
+	@Test
+	void testDivideZero() {
+		int a = 10;
+		int b = 0;
+		assertThrows(ArithmeticException.class, () -> {
+			int c = a / b;
+		});
+	}
+
+	@Test
+	void testListContains() {
+		assertTrue(testList.contains(1));
+		testList.remove(0);
+		assertFalse(testList.contains(1));
 	}
 
 }
