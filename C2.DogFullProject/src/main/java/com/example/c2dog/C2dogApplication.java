cd1a7e9fcacb28351198061818d6d6f2d4005641
KhaledM0hamed <khaledmzakria1@gmail.com>
2023-01-13 13:30:45 +0200

    init C2DOG

---
+++ C2.DogFullProject/src/main/java/com/example/c2dog/C2dogApplication.java
@@ -0,0 +1,13 @@
+package com.example.c2dog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C2dogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C2dogApplication.class, args);
+	}
+
+}
---
+++ C2.DogFullProject/src/test/java/com/example/c2dog/C2dogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.c2dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C2dogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
