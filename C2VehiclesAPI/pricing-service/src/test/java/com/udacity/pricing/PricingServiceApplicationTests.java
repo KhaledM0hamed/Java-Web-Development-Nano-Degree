4352712360a42db57a8e239aa59c4987077c3b7b
KhaledM0hamed <khaledmzakria1@gmail.com>
2023-01-19 11:27:44 +0200

    Convert the Pricing Service and one test

--- C2VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ C2VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,40 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.api.PricingController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringRunner.class)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
+@WebMvcTest(PricingController.class)
 public class PricingServiceApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Before
+	public void Setup(){
+		mockMvc = MockMvcBuilders.standaloneSetup(new PricingController()).build();
+	}
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void priceTest() throws Exception {
+		this.mockMvc.perform(get("/services/price?vehicleId=1")).andExpect(status().isOk());
 	}
 
 }
